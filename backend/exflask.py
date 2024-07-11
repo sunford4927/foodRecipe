bbe2c15202b1bf632188294ff629b62595462b72
jw <jw39@gmail.com>
2024-07-11 19:00:43 +0900

    지원 푸시 이거 되는 거 맞냐1

--- backend/exflask.py
+++ backend/exflask.py
@@ -1,4 +1,4 @@
-
+# from urllib import parse
 
 # pip install pymysql
 # pip install cryptography 설치해야됨
@@ -34,7 +34,7 @@ def setQuery(sql=None, data = None):
         user='root',
         password='1234',
         db='maven', 
-        charset='utf8'
+        charset='utf8mb4'
     )
     
     # 데이터에 접근
@@ -54,19 +54,25 @@ def setQuery(sql=None, data = None):
 
 
 import pymysql
+import json
 # pip install flask_restx
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for, make_response
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 # app = Flask(__name__)
 app = Flask(__name__, static_folder='../build', static_url_path='/') # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+# 인코딩위해 잠깐 테스트
+app.config['JSON_AS_ASCII'] = False
 api = Api(app)  # Flask 객체에 Api 객체 등록
 #===========================================
 
 # Get 은 정보 조회
 @api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
+    
+
     def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
         data = setQuery("select * from board")
+        
         return jsonify({'result': data})
     
     def post(self):
@@ -76,38 +82,29 @@ class HelloWorld(Resource):
         input_img = request.form['img']
         sql = "INSERT INTO board(title, content, writer, img) VALUES(%s, %s, %s, %s)"
         value = (input_id, input_pw, input_nm, input_img )
+        # value = parse.unquote(value, 'utf8')
         setQuery(sql, value)
-        return jsonify({'result': value})
+        # 한글 인코딩 수정
+        response = make_response(json.dumps({'result': value}, ensure_ascii=False))  # JSON 응답 생성 및 한글 깨짐 방지 설정
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'  # 응답 헤더 설정
+        return response
         
         
     
     def delete(self):  # DELETE 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-
+        #input_DT = request.form['delTest']
+        print("getjson : ",request.get_json()['title'])
+        # 키 꺼내는 방법 ['']
+        print("data : ",request.data)
         
-        #input_name = request.form['input_name']
-        #input_phone = request.form['input_phone']
-        #print(input_name, input_phone)
-        # MySQL 데이터베이스 연결
-        db = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='maven', charset='utf8')
-
-        # 데이터에 접근
-        cursor = db.cursor()
-        
-        # SQL query 작성
         sql = "DELETE FROM board WHERE title=%s"
         #("delete from LIKELIST where lk_senid = #{lk_senid} and lk_recid = #{lk_recid}")    
-        value = ("플라스크제목")
+        value = request.get_json()['title']
+        setQuery(sql,value)
+        response = make_response(json.dumps({'hello': value}, ensure_ascii=False))  # JSON 응답 생성 및 한글 깨짐 방지 설정
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'  # 응답 헤더 설정
+        return response
         
-        # 쿼리 실행
-        cursor.execute(sql,value)
-
-        
-        db.commit()
-        # cursor.execute("select * from board")
-        # data = cursor.fetchall()
-        db.close()
-
-        return {"hello": ""}
 
 #================================================
 if __name__ == '__main__':
