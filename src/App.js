e02950a274e5323c842f5b4a16999d52da5d3d4c
sunford <p01097220945@gmail.com>
2024-07-15 20:47:54 +0900

    상현 다크모드 리덕스 상태 추가완료

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@
 import { Routes, Route } from 'react-router-dom';
 import FrontBoard from './pages/FrontBoard';
 import Counter from './pages/Counter';
-
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { modechange } from './redux/actions';
+import './style/App.css'
+import dark from './img/dark.png'
+import light from './img/light.png'
 
 function App() {
+  const modeState = useSelector(state => state.backMode)
+  const dispatch = useDispatch(modechange());
+
   return (
-    <div className="App">
+    <div id={modeState? "darkMode" : "lightMode"}>
+        <button id="modeBtn" onClick={()=>dispatch(modechange())}>
+          <img src={modeState? dark : light}/>
+
+        </button>
+      
         <header>
           헤더 페이지
         </header>
