55376aea2fb0111adca23abd30777f8f36288a76
sunford <p01097220945@gmail.com>
2024-07-18 19:28:45 +0900

    상현 메인보드 화면 수정

---
+++ src/components/customhook/pagination/Pagination.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import "./Pagination.scss"
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Pagination = ({ pageCount, onPageChange, currentPage }) => {
+    return (
+        <ReactPaginate
+            pageRangeDisplayed={4}
+            marginPagesDisplayed={1} // 여백에 표시할 페이지 수입니다.
+            previousLabel={<FiChevronLeft />}
+            nextLabel={<FiChevronRight />}
+            pageCount={pageCount} // 필수. 총 페이지 수.
+            onPageChange={onPageChange}
+            containerClassName={"pagination"}
+            renderOnZeroPageCount={currentPage}
+            pageLinkClassName={"pagination__link"}
+            activeLinkClassName={"pagination__link__active"}
+            
+        />  
+    );
+};
+
+export default Pagination;
