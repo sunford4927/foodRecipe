4fab6714fa3505147b3d2e27d5fe821e4146e275
sunford <p01097220945@gmail.com>
2024-07-22 16:40:09 +0900

    상현 네트워크 주소 변경

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+// const { createProxyMiddleware }  = require('http-proxy-middleware')
+
+// module.exports = function (app){
+//     app.use(
+//         '/MainBoard',
+//         createProxyMiddleware({
+//             target : 'http://192.168.219.111:5000',
+//             changeOrigin : true,
+//         })
+//     )
+// }
