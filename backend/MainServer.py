8a86656f42bd857f348f2947bfed7901a672c33c
jw <jw39@gmail.com>
2024-07-29 17:08:18 +0900

    지원 메인서버 수정

---
+++ backend/MainServer.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restx import Api
+from flask_cors import CORS
+# from MainBoard import AllInfo   # 각 클래스 import
+from MainBoard import MainBoard, AllInfo 
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+CORS(app)  # CORS 설정 추가
+app.config['JSON_AS_ASCII'] = False
+api = Api(app)
+
+
+# import한 py파일 중 클래스를 API에 추가.. 경로 지정한 겨
+api.add_resource(AllInfo, '/all_info')
+api.add_resource(MainBoard, '/MainBoard')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ backend/Rank.py
@@ -0,0 +1,4 @@
+from flask_restx import Resource
+from flask import jsonify, request
+from db_utils import setQuery #setQuery 가져오기
+
