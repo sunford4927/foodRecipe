ba1d5232d1145c744a4489a6593a96e93b2a29b3
jw <jw39@gmail.com>
2024-07-29 19:44:34 +0900

    지원 카테고리별 출력 클래스 추가

--- backend/MainServer.py
+++ backend/MainServer.py
@@ -3,6 +3,8 @@ from flask_restx import Api
 from flask_cors import CORS
 # from MainBoard import AllInfo   # 각 클래스 import
 from MainBoard import MainBoard, AllInfo 
+from RankBoard import getrank
+from categoryBoard import category
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app)  # CORS 설정 추가
@@ -13,6 +15,8 @@ api = Api(app)
 # import한 py파일 중 클래스를 API에 추가.. 경로 지정한 겨
 api.add_resource(AllInfo, '/all_info')
 api.add_resource(MainBoard, '/MainBoard')
+api.add_resource(getrank, '/getrank')
+api.add_resource(category, '/category_board')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
