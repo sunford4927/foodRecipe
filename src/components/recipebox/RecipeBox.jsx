4c763edbdaa0c52110721b47d7bee3b1335d6a9c
sunford <p01097220945@gmail.com>
2024-07-29 17:58:59 +0900

    상현 오류 수정

--- src/components/recipebox/RecipeBox.jsx
+++ src/components/recipebox/RecipeBox.jsx
@@ -4,7 +4,7 @@ import { setView } from '../../util/util';
 const RecipeBox = ( {total, data} ) => {
     return (
         <>
-            <p>총 {total}개의 맛있는 레시피가 있습니다.</p>
+            {total != 0 && <p>총 {total}개의 맛있는 레시피가 있습니다.</p>}
             <div className='recipeContainer'>
                     {data.length > 0 &&  setView(data)}
             </div>
