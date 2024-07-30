190a8438060b6e6c799c0171d5d757b93a5c7192
sunford <p01097220945@gmail.com>
2024-07-30 10:48:17 +0900

    상현 기능추가 및 테스트 화면 추가

--- src/pages/frontBoard/FrontBoard.jsx
+++ src/pages/frontBoard/FrontBoard.jsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import Dummy from '../../components/test/Dummy';
 import './FrontBoard.scss'
-import Up from '../../img/상승.png'
-import Down from '../../img/하강.png'
+
 import Pagination from '../../components/customhook/pagination/Pagination';
 import { sendGet, URL } from '../../util/util';
 import { useNavigate } from 'react-router-dom';
 import RecipeBox from '../../components/recipebox/RecipeBox';
+import { useDispatch } from 'react-redux';
+import CategoryTable from '../../components/categorytable/CategoryTable';
+import { addCategoryTag, clearCategory } from '../../redux/actions';
 
 
 
 const FrontBoard = () => {
-    const [viewTable, setViewTable] = useState(true);
     const [mainBoard, setMainBoard] = useState([]);
     const [totalData, setTotalData] = useState(0);
     const [maxPage, setMaxPage] = useState(0);
-    
+
     const nav = useNavigate();
-  
+    const dispatch = useDispatch();
+
+    const dummyList = [
+        <Dummy List={"type"} idx={1}/>,
+        <Dummy List={"state"} idx={2}/>,
+        <Dummy List={"ingredient"} idx={3}/>,
+        <Dummy List={"method"} idx={4}/>
+    ];
 
     let curPage = 0;
-    
-    function initPageCount(data){
+
+    function initPageCount(data) {
         setTotalData(data[0].totalCnt);
-        setMaxPage(Math.floor(parseInt(data[0].totalCnt)/100));     
+        setMaxPage(Math.floor(parseInt(data[0].totalCnt) / 100));
     }
 
-    useEffect(()=>{
-        sendGet(URL+"/all_info", initPageCount);
-        sendGet(URL+"/MainBoard?page=1", setMainBoard);  
-    },[])
+    useEffect(() => {
+        sendGet(URL + "/all_info", initPageCount);
+        sendGet(URL + "/MainBoard?page=1", setMainBoard);
+        dispatch(addCategoryTag(dummyList));
+        dispatch(clearCategory());
+    }, [])
 
     // 비동기 post 요청 및 데이터 전송 예제
     // function test(){
@@ -48,17 +58,17 @@ const FrontBoard = () => {
     //     )
     // }
 
-    
+
     // useEffect(() => {
     //     axios
-        
+
     //         .get("hello")
     //         .then((res) =>{
     //             setValue(res);
 
     //         }
     //         )
-        
+
     //         // fetch("hello")
     //         // .then((res)=> {
     //         //     console.log(res);
@@ -66,72 +76,28 @@ const FrontBoard = () => {
     //         //  })
     //         // .then((res)=> {
     //         //     console.log(res);
-                
-                
+
+
     //         // })
     //         // .catch(() => {
-    
+
     //         // })
     // },[])
-    
-    function handlePageChange(e){
-        curPage = e.selected+1;
-        sendGet(URL+"/MainBoard?page="+curPage, setMainBoard)
+
+    function handlePageChange(e) {
+        curPage = e.selected + 1;
+        sendGet(URL + "/MainBoard?page=" + curPage, setMainBoard)
     }
-    
+
 
     return (
         <div className='inner'>
-            {viewTable ? 
-            <table width={"100%"} border={1} id='table'>
-                <colgroup>
-                    <col style={{width:"100px"}}/>
-                    <col />
-                </colgroup>
-                <tbody>
-                    <tr>
-                        <th>
-                            <div><span className='cursor'>종류별</span></div>
-                            <div><span className='cursor'>상황별</span></div>
-                            <div><span className='cursor'>재료별</span></div>
-                            <div><span className='cursor'>방법별</span></div>
-                        </th>
-                        <td>
-                            <div className='rcp_cate st3'>
-                                <div><Dummy List={"type"}/></div>
-                                <div><Dummy List={"state"}/></div>
-                                <div><Dummy List={"ingredient"}/></div>
-                                <div><Dummy List={"method"}/></div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            :
-            <></>
-            }
-
-            
-            <div id='viewController' onClick={()=>{setViewTable(!viewTable)}}> 
-                    {viewTable ? 
-                    <div>
-                        카테고리 닫기 
-                        <img className='icon' src={Up} alt="" /> 
-                    </div>
-                    :
-                    <div>
-                        카테고리 열기
-                        <img className='icon' src={Down} alt="" /> 
-                    </div>
-                    }
-            </div>
-            
-            <RecipeBox total={totalData} data={mainBoard}/>
-
+            <CategoryTable />
+            <RecipeBox total={totalData} data={mainBoard} />
             <Pagination
-                        pageCount={maxPage}
-                        onPageChange={(e)=>handlePageChange(e)}
-                        currentPage={curPage}
+                pageCount={maxPage}
+                onPageChange={(e) => handlePageChange(e)}
+                currentPage={curPage}
             />
 
         </div>
