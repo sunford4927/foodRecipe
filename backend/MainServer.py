40553c1d15d6309a32a2b229e4c14a79c95a7ca4
jw <jw39@gmail.com>
2024-07-30 13:48:44 +0900

    지원 랭크 수정 및 POST TEST

--- backend/MainServer.py
+++ backend/MainServer.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
-# from MainBoard import AllInfo   # 각 클래스 import
+
+from hello import test
 from MainBoard import MainBoard, AllInfo 
 from RankBoard import getrank
-from categoryBoard import category
+from CategoryBoard import category
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app)  # CORS 설정 추가
@@ -13,6 +14,8 @@ api = Api(app)
 
 
 # import한 py파일 중 클래스를 API에 추가.. 경로 지정한 겨
+api.add_resource(test, '/HelloTest')
+# api.add_resource(test, '/deltest')
 api.add_resource(AllInfo, '/all_info')
 api.add_resource(MainBoard, '/MainBoard')
 api.add_resource(getrank, '/getrank')
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,12 +1,40 @@
-import React, { useEffect } from 'react';
-import { sendPost } from './util/util';
+import React, { useEffect, useState } from 'react';
+import { sendDel, sendPost, URL } from './util/util';
+
+
+
+
 
 const Test = () => {
-    useEffect(()=>{
-        sendPost("11", null, { type : 1, data :2})
-    },[])
+    const [value1 ,setValue1] = useState("");
+    const [value2 ,setValue2] = useState("");
+    const [value3 ,setValue3] = useState("");
+    const [value4 ,setValue4] = useState("");
+    // useEffect(()=>{
+    //     sendDel(URL + "/HelloTest", null, 
+    //         { name:"test6"})
+    // },[])
+
+
+
+    function sendGreenLite(){
+        sendPost(
+            URL + "/HelloTest",
+            null, 
+            {name:value1, id:value2 , pw:value3 , nm:value4}
+        )
+        console.log(1)
+        // console.log(value1);
+    }
+    // /test
+
     return (
         <div>
+            <input type="text" onChange={(e)=> setValue1(e.target.value)} />
+            <input type="text" onChange={(e)=> setValue2(e.target.value)} />
+            <input type="text" onChange={(e)=> setValue3(e.target.value)} />
+            <input type="text" onChange={(e)=> setValue4(e.target.value)} />
+            <button onClick={sendGreenLite}>제출</button>
             Test 화면        
         </div>
     );
