2b9b725808a9d2e859569e6ba005ead4099f041d
sunford <p01097220945@gmail.com>
2024-07-30 17:43:47 +0900

    상현 카테고리 버튼기능 추가

--- src/components/test/Dummy.jsx
+++ src/components/test/Dummy.jsx
@@ -1,53 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { pathToStr } from '../../util/util';
-import { useDispatch } from 'react-redux';
+import { pathToStr, strToPath } from '../../util/util';
+import { useDispatch, useSelector } from 'react-redux';
 import { addCategory } from '../../redux/actions';
 
 const Dummy = ({List, idx}) => {
     const data = {
-        type : ["전체", "밑반찬", "국/탕", "찌개", "디저트", "면/만두", "밥/죽/떡", "퓨전", "김치/젓갈/장류", "양념/소스/잼", "양식", "샐러드", "스프", "빵", "과자", "차/음료/술", "기타"],
+        type : ["전체", "밑반찬", "메인반찬", "국/탕", "찌개", "디저트", "면/만두", "밥/죽/떡", "퓨전", "김치/젓갈/장류", "양념/소스/잼", "양식", "샐러드", "스프", "빵", "과자", "차/음료/술", "기타"],
         state : ['전체', '일상', '초스피드', '손님접대', '술안주', '다이어트', '도시락', '영양식', '간식', '야식', '푸드스타일링', '해장', '명정', '명절', '이유식', '기타'],
         ingredient : ['전체', '소고기', '돼지고기', '닭고기', '육류', '채소류', '해물류', '달걀/유제품', '가공식품류', '쌀', '밀가루', '건어물류', '버섯류', '과일류', '콩/견과류', '곡류', '기타'],
         method : ['전체', '볶음', '끓이기', '부침', '조림', '무침', '비빔', '찜', '절임', '튀김', '삶기', '굽기', '데치기', '회', '기타']
     }[List]
     const dispatch = useDispatch();
-    
+    const selectdic = useSelector(state => state.category)
 
-    const [curValue, setCurValue] = useState("")
+    const [curValue, setCurValue] = useState(null)
     function onClickCategory(e){
-        curValue.className = curValue.className.replace('first_category', '');
-        e.target.className += ' first_category';
-        setCurValue(e.target);
-    }
-    useEffect(() => {
-        let list = document.getElementsByClassName("category"+idx)
-        for(let i =0 ; i< list.length; i++)
+        if(curValue !=null)
         {
-            if(list[i].innerText == "전체")
-            {
-                list[i].className += " first_category"
-                setCurValue(list[i])
-            }
+            curValue.className = curValue.className.replaceAll('first_category', '');
         }
-  
-    },[])
+        e.target.className += ' first_category';
+        setCurValue(e.target)
+        dispatch(addCategory(idx,pathToStr(e.target.innerText)))
+        
+    }
+
+
 
     useEffect(()=>{
-        dispatch(addCategory(idx,curValue.innerText))
-        console.log(1)
-    },[curValue])
+
+        if(curValue != null)
+        {
+            curValue.className = curValue.className.replaceAll('first_category', '');
+        }
+        let value = "";
+        switch(idx)
+        {
+            case 1:
+                value = "CK_KIND_NM";
+                break;
+            case 2:
+                value = "CK_STA_NM";
+                break;
+            case 3:
+                value = "CK_INPUT_NM";
+                break;
+            case 4:
+                value = "CK_ACT_NM"
+                break;
+        }
+
+        let list = document.getElementsByClassName("category"+idx)
+        for(let i=0 ; i< list.length; i++)
+        {
+            if(list[i].innerText == strToPath(selectdic[value]))
+            { 
+                list[i].className += " first_category"
+                setCurValue(list[i])
+                break;
+            }
+        }
+    
+    
+      
+    },[selectdic])
     const nav = useNavigate();
     return (
         <>
             {data && data.map((item)=>{
                 if(item===1)
                 {
-                    return <Link key={item} to={`/category/${pathToStr(item)}`}><span onClick={(e)=>onClickCategory(e)} className={`cursor ${"category"+idx}`}>{item}</span></Link>
+                    return <Link key={item} to={`/category/${"&"+pathToStr(item)}`}><span onClick={(e)=>onClickCategory(e)} className={`Text cursor ${"category"+idx}`}>{item}</span></Link>
                     //return <span onClick={(e)=>onClickCategory(e)} className={`cursor ${"category"+idx}`}>{item}</span>
                 }
                 else{
-                    return <Link key={item} to={`/category/${pathToStr(item)}`}><span onClick={(e)=>onClickCategory(e)} className={`cursor ${"category"+idx}`} >{item}</span></Link>
+                    return <Link key={item} to={`/category/${"&"+pathToStr(item)}`}><span onClick={(e)=>onClickCategory(e)} className={`Text cursor ${"category"+idx}`} >{item}</span></Link>
                     //return <span onClick={(e)=>onClickCategory(e)} className={`cursor ${"category"+idx}`} >{item}</span>
                 }
             })}   
--- src/pages/categoryrecipe/CategoryRecipe.jsx
+++ src/pages/categoryrecipe/CategoryRecipe.jsx
@@ -1,20 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeBox from '../../components/recipebox/RecipeBox';
-import { useParams } from 'react-router-dom';
-import { strToPath } from '../../util/util';
+import { useNavigate, useParams } from 'react-router-dom';
+import { sendGet, strToPath, URL } from '../../util/util';
 import { useSelector } from 'react-redux';
 import CategoryTable from '../../components/categorytable/CategoryTable';
+import Pagination from '../../components/customhook/pagination/Pagination';
+
+const DATAPATH = '/category_board?&page=';
+const TOTALPATH = '/category_cnt?&page=';
+
+function strCheck(str){
+    let isCheck = false;
+    for(let key in str)
+    {
+        if(str[key]!= "전체")
+        {
+            isCheck = true;
+            break;
+        }
+    }
+    return isCheck;
+}
 
 
 const CategoryRecipe = () => {
-    let { value } = useParams();
-    console.log(value)
-    
+    //let { value } = useParams();
+    const dic = useSelector(state => state.category)
+    const [mainBoard, setMainBoard] = useState([]);
+    const [totalData, setTotalData] = useState(0);
+    const [maxPage, setMaxPage] = useState(0);
+
+    const nav = useNavigate();
+
+    let curPage = 1;
+
+    function initPageCount(data) {
+        setTotalData(data[0].CATE_CNT);
+        setMaxPage(Math.floor(parseInt(data[0].CATE_CNT) / 100));
+    }
+
+    function setUrl(idx, path){
+        let url = URL+path+ parseInt(idx);
+        for(let key in dic)
+        {
+            url += "&" + key + "=" + dic[key];
+        }
+        return url;
+    }
+    useEffect(()=>{
+        if(strCheck(dic))
+        {
+            sendGet(setUrl(1, TOTALPATH), initPageCount);
+            sendGet(setUrl(1, DATAPATH), setMainBoard, null)
+        }
+        
+    },[])
+
+    useEffect(()=>{
+        console.log(dic)
+        let isCheck = strCheck(dic)
+        if(isCheck)
+        {
+            sendGet(setUrl(curPage, TOTALPATH), initPageCount);
+            sendGet(setUrl(curPage,DATAPATH), setMainBoard, null)
+        }
+        else{
+            //sendGet(URL + "/MainBoard?page=1", setMainBoard);
+            nav('/');
+        }
+        
+
+    }, [dic])
+
+    function setText()
+    {
+        let isCheck = false;
+        let value = "";
+        for(let key in dic)
+        {
+            if(dic[key]!= "전체")
+            {
+                if(!isCheck)
+                {
+                    value += strToPath(dic[key]);
+                    isCheck = true; 
+                }
+                else
+                {
+                    value += " > " + strToPath(dic[key]);
+                }
+            }
+        }
+        return value;
+    }
+
+
+    function handlePageChange1(e) {
+        curPage = e.selected + 1;
+        sendGet(setUrl(curPage, DATAPATH), setMainBoard)
+    }
+
     return (
         <div className='inner'>
             <CategoryTable/>
-            <p>전체 {">"} {strToPath(value)}</p>
-            {/* <RecipeBox/> */}
+            <p>전체 {">"} {setText()}</p>
+            <RecipeBox total={totalData > 0 && totalData} data={mainBoard.length > 0 &&mainBoard} />
+            <Pagination
+                pageCount={maxPage > 0 && maxPage}
+                onPageChange={(e) => handlePageChange1(e)}
+                currentPage={curPage}
+            />
         </div>
     );
 };
