e69f1f27bf4893ab483afda18cdd959250f3ee63
jw39 <akdlwb1212@naver.com>
2024-07-30 14:22:55 +0900

    지원 프론트 랭크 수정 및 이것저것 수정

--- backend/categoryBoard.py
+++ backend/categoryBoard.py
@@ -4,25 +4,16 @@ from db_utils import setQuery
 
 class category(Resource):
     def get(self):
-        # cateDic = request.args.to_dict()
-
-        # 테스트 용! 프론트 준비되면 삭제
-        cateDic = {
-
-            "CK_KIND_NM" : "밑반찬",
-            "CK_STA_NM" : "일상",
-            "CK_INPUT_NM" : "소고기",
-            "CK_ACT_NM" : "볶음"
-        }
+        cateDic = request.args.to_dict()
 
         isInput = False
         query = """SELECT DISTINCT CK_KIND_NM, CK_STA_NM, CK_INPUT_NM, CK_ACT_NM, R.RCP_SNO, 
                     RCP_TTL, USER_NM, VIEW_CNT, REVIEW_CNT , SCORE_AVG 
                     from RECIPE_BOARD R join BOARD_INFO B
                     on R.RCP_SNO = B.RCP_SNO WHERE """
-        
+  
         for ckNM in cateDic :
-            if (cateDic[ckNM] != "") :
+            if (cateDic[ckNM] != "전체") :
                 if (isInput) :
                     query += f"AND {ckNM} LIKE '{cateDic[ckNM]}'"
                 else:
@@ -30,7 +21,7 @@ class category(Resource):
                     isInput = True
         
         data = setQuery(query)
-        
+
         return jsonify(data)
 
         
