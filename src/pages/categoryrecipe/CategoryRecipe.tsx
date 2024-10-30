bd0b43fe334de1128585cabef54aaa9b1310ec44
sunford <sunford4927@gmail.com>
2024-10-30 20:12:07 +0900

    상현 타입스크립트 에러 수정중

--- src/pages/categoryrecipe/CategoryRecipe.tsx
+++ src/pages/categoryrecipe/CategoryRecipe.tsx
@@ -1,123 +1,92 @@
 import React, { useEffect, useState } from 'react';
 import RecipeBox from '../../components/recipebox/RecipeBox';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { sendGet, strToPath, URL } from '../../util/util';
 import { useSelector } from 'react-redux';
 import CategoryTable from '../../components/categorytable/CategoryTable';
 import Pagination from '../../components/customhook/pagination/Pagination';
 
+// 데이터 경로 상수
 const DATAPATH = '/category_board?&page=';
 const TOTALPATH = '/category_cnt?&page=';
 
-function strCheck(str){
-    let isCheck = false;
-    for(let key in str)
-    {
-        if(str[key]!= "전체")
-        {
-            isCheck = true;
-            break;
-        }
-    }
-    return isCheck;
+// 문자열 확인 함수
+function strCheck(str: Record<string, string>): boolean {
+    return Object.values(str).some(value => value !== "전체");
 }
 
-
-const CategoryRecipe = () => {
-    //let { value } = useParams();
-    const dic = useSelector(state => state.category)
-    const [mainBoard, setMainBoard] = useState([]);
-    const [totalData, setTotalData] = useState(10);
-    const [maxPage, setMaxPage] = useState(0);
+const CategoryRecipe: React.FC = () => {
+    const dic = useSelector((state: any) => state.category);
+    const [mainBoard, setMainBoard] = useState<any[]>([]);
+    const [totalData, setTotalData] = useState<number>(10);
+    const [maxPage, setMaxPage] = useState<number>(0);
 
     const nav = useNavigate();
-
     let curPage = 1;
 
-    function initPageCount(data) {
+    // 페이지 카운트 초기화
+    function initPageCount(data: any[]): void {
         console.log(data);
-        setTotalData(data[0].CATE_CNT);
-        setMaxPage(Math.floor(parseInt(data[0].CATE_CNT) / 100));
+        setTotalData(data[0]?.CATE_CNT || 0);
+        setMaxPage(Math.floor((data[0]?.CATE_CNT || 0) / 100));
     }
 
-    function setUrl(idx, path){
-        let url = URL+path+ parseInt(idx);
-        for(let key in dic)
-        {
+    // URL 설정 함수
+    function setUrl(idx: number, path: string): string {
+        let url = URL + path + idx;
+        for (let key in dic) {
             url += "&" + key + "=" + dic[key];
         }
         return url;
     }
-    useEffect(()=>{
-        if(strCheck(dic))
-        {
-            sendGet(setUrl(1, TOTALPATH), initPageCount);
-            sendGet(setUrl(1, DATAPATH), setMainBoard, null)
-        }
-        
-    },[])
 
-    useEffect(()=>{
-        console.log(totalData);
-    },[totalData])
-    useEffect(()=>{
-        console.log(dic)
-        let isCheck = strCheck(dic)
-        if(isCheck)
-        {
-            sendGet(setUrl(curPage, TOTALPATH), initPageCount);
-            sendGet(setUrl(curPage,DATAPATH), setMainBoard, null)
+    useEffect(() => {
+        if (strCheck(dic)) {
+            sendGet(setUrl(1, TOTALPATH), initPageCount);
+            sendGet(setUrl(1, DATAPATH), setMainBoard, null);
         }
-        else{
-            //sendGet(URL + "/MainBoard?page=1", setMainBoard);
+    }, []);
+
+    useEffect(() => {
+        console.log(totalData);
+    }, [totalData]);
+
+    useEffect(() => {
+        console.log(dic);
+        if (strCheck(dic)) {
+            sendGet(setUrl(curPage, TOTALPATH), initPageCount);
+            sendGet(setUrl(curPage, DATAPATH), setMainBoard, null);
+        } else {
             nav('/');
         }
-        
+    }, [dic]);
 
-    }, [dic])
-
-    function setText()
-    {
-        let isCheck = false;
-        let value = "";
-        for(let key in dic)
-        {
-            if(dic[key]!= "전체")
-            {
-                if(!isCheck)
-                {
-                    value += strToPath(dic[key]);
-                    isCheck = true; 
-                }
-                else
-                {
-                    value += " > " + strToPath(dic[key]);
-                }
-            }
-        }
-        return value;
+    // 텍스트 설정 함수
+    function setText(): string {
+        return Object.values(dic)
+            .filter(value => value !== "전체")
+            .map(value => strToPath(value as string))
+            .join(' > ');
     }
 
-
-    function handlePageChange1(e) {
+    // 페이지 변경 핸들러
+    function handlePageChange1(e: { selected: number }): void {
         curPage = e.selected + 1;
-        sendGet(setUrl(curPage, DATAPATH), setMainBoard)
+        sendGet(setUrl(curPage, DATAPATH), setMainBoard);
     }
 
     return (
         <div className='inner'>
-            <CategoryTable/>
+            <CategoryTable />
             <p>전체 {">"} {setText()}</p>
-            <RecipeBox total={totalData > 0 && totalData} data={mainBoard.length > 0 &&mainBoard} />
+            <RecipeBox total={totalData > 0 ? totalData : 0} data={mainBoard.length > 0 ? mainBoard : []} />
             <Pagination
-                pageCount={maxPage > 0 && maxPage}
-                onPageChange={(e) => handlePageChange1(e)}
+                pageCount={maxPage > 0 ? maxPage : 0}
+                onPageChange={handlePageChange1}
                 currentPage={curPage}
             />
         </div>
     );
 };
 
-
 export default CategoryRecipe;
-
