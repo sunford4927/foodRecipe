deee1fe0e54541f1be4b7fa462d577a41b336529
sunford <p01097220945@gmail.com>
2024-10-30 21:03:07 +0900

    10/30 상현 app.ts  수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.js';
+import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -14,7 +14,7 @@ import { Provider } from 'react-redux';
 
 // const store = createStore(counter);
 const store = configureStore({
-  reducer: counterReducer,
+    reducer: counterReducer,
 });
 
 
@@ -31,13 +31,13 @@ const store = configureStore({
 // npm i http-proxy-middleware
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  //<React.StrictMode>
-  <Provider store={store}>
+    //<React.StrictMode>
+    <Provider store={ store } >
     <BrowserRouter>
-      <App />
+    <App />
     </BrowserRouter>
-  </Provider>
-  //</React.StrictMode>
+</Provider>
+    //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
