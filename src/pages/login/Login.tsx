e959819dddbe7b037f6fb07753c1467f5c928a9c
sunford <sunford4927@gmail.com>
2024-10-30 20:34:20 +0900

    10/30 상현 타입스크립트 오류 수정 완료

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,80 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import { changeState, getCurUser, loginEmail, loginGoogle, logout } from '../../util/auth/firebase';
-import './Login.scss'
-import googleIcon from "../../img/구글아이콘.png"
+import './Login.scss';
+import googleIcon from "../../img/구글아이콘.png";
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setuserinfo } from '../../redux/actions';
 
-// 파이어베이스 데이터 베이스 확인 주소
-// /https://console.firebase.google.com/project/testbase-9e244/overview?hl=ko
-
-
-const Login = () => {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [isLogin, setIsLogin] = useState(false);
+const Login: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [isLogin, setIsLogin] = useState<boolean>(false);
     const dispatch = useDispatch();    
     const nav = useNavigate();
 
-    
-    function checkLogin(){
+    function checkLogin() {
         logout();
         changeState(setIsLogin);
     }
 
-    useEffect(()=>{
-        
+    useEffect(() => {
         checkLogin();
-    },[])
+    }, []);
+
     return (
         <div className='Logininner'>
             <div className='inner login'>
                 <h2>로그인</h2>
                 
-                <input type="text" onChange={e => setEmail(e.target.value)} placeholder='아이디' name="email"/> <br/>
-                <input type="password" name='pw' onChange={e => setPassword(e.target.value)}placeholder='비밀번호'/>  <br/> 
-                <div style={{display: "flex", justifyContent : "space-around"}}>
-                    <button className='blueBtn twoBtn bHover' onClick={(e)=>{nav("/join")}
-                                                } >회원가입</button>    
-                    <button className='blueBtn twoBtn bHover' onClick={()=>{
-                                                        isLogin ?
-                                                        logout() :
-                                                        loginEmail(email,password).then(res => {
-                                                            dispatch(setuserinfo(res.user));
-                                                            
-                                                            nav("/");
-                                                        })                                                      
-                                                        .catch((error) => {
-                                                            console.log(error.code)
-                                                            if(error.code == "auth/missing-email")
-                                                            {
-                                                                alert("이메일을 잘못 입력하셧습니다")
-                                                            }
-                                                            else if(error.message == "auth/invalid-credential")
-                                                            {
-                                                                alert("간편로그인으로 가입된 사용자 입니다")
-                                                            }
-                                                        });
-                                                    }
-                                                } >{isLogin?"로그아웃" : "로그인"}</button> 
+                <input 
+                    type="text" 
+                    onChange={e => setEmail(e.target.value)} 
+                    placeholder='아이디' 
+                    name="email" 
+                /> <br/>
+                <input 
+                    type="password" 
+                    name='pw' 
+                    onChange={e => setPassword(e.target.value)} 
+                    placeholder='비밀번호' 
+                />  <br/> 
+                <div style={{display: "flex", justifyContent: "space-around"}}>
+                    <button 
+                        className='blueBtn twoBtn bHover' 
+                        onClick={() => nav("/join")}
+                    >
+                        회원가입
+                    </button>    
+                    <button 
+                        className='blueBtn twoBtn bHover' 
+                        onClick={() => {
+                            if (isLogin) {
+                                logout();
+                            } else {
+                                loginEmail(email, password)
+                                    .then(res => {
+                                        dispatch(setuserinfo(res.user));
+                                        nav("/");
+                                    })
+                                    .catch((error) => {
+                                        console.log(error.code);
+                                        if (error.code === "auth/missing-email") {
+                                            alert("이메일을 잘못 입력하셨습니다");
+                                        } else if (error.code === "auth/invalid-credential") {
+                                            alert("간편로그인으로 가입된 사용자입니다");
+                                        }
+                                    });
+                            }
+                        }}
+                    >
+                        {isLogin ? "로그아웃" : "로그인"}
+                    </button> 
                 </div> 
                 <br />
-                <div style={{display : 'flex', justifyContent : 'center'}}>
-                    <button className='bHover' onClick={()=>{
-                                            loginGoogle()
-                                            .then(function (result) {
-                                                console.log(result);
-                                                dispatch(setuserinfo(result));
-                                                nav("/");
-                                            })
-                                            .catch(function (error) {
-                                                
-                                            console.error(error);
-                                            })
-                                        }
-                                    }>
-                        <img className='icon' src={googleIcon}/>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <button 
+                        className='bHover' 
+                        onClick={() => {
+                            loginGoogle()
+                                .then(result => {
+                                    console.log(result);
+                                    dispatch(setuserinfo(result));
+                                    nav("/");
+                                })
+                                .catch(error => {
+                                    console.error(error);
+                                });
+                        }}
+                    >
+                        <img className='icon' src={googleIcon} alt="Google Icon" />
                     </button>
                 </div>
             </div>
