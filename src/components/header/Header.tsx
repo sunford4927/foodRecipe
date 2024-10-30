80e4e177dd099623cd49ff9b5fb87c7d2a4d2916
sunford <sunford4927@gmail.com>
2024-10-30 20:34:20 +0900

    10/30 상현 타입스크립트 오류 수정 완료

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,55 +1,61 @@
 import React, { useEffect } from 'react';
-import SearchOk from '../../img/돋보기.png'
-import Profile from '../../img/프로필.png'
-import MenuBtn from '../../img/메뉴.png'
-import logo from '../../img/logo.png'
-import './Header.scss'
+import SearchOk from '../../img/돋보기.png';
+import Profile from '../../img/프로필.png';
+import MenuBtn from '../../img/메뉴.png';
+import logo from '../../img/logo.png';
+import './Header.scss';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+const size = {
+    width: '40px',
+    height: '40px'
+};
 
-const size ={
-    width : '40px',
-    height : '40px'
+interface User {
+    displayName: string;
 }
 
-const Header = () => {
+const Header: React.FC = () => {
     const nav = useNavigate();
-    const isLogin = useSelector(state => state.isLogin);
-    let user = useSelector(state => state.user);
-    
+    const isLogin = useSelector((state: any) => state.isLogin);
+    const user = useSelector((state: { user: User | null }) => state.user);
 
-    useEffect(()=>{
-        console.log(isLogin)
-    },[isLogin])
+    useEffect(() => {
+        console.log(isLogin);
+    }, [isLogin]);
+
     return (
         <div id='headerLine'>
             <div id='topBox'>
-                <img className='cursor' src={logo} onClick={() => nav('/')} alt="만개레시피 제목" style={{width:280, height:90}} />
+                <img 
+                    className='cursor' 
+                    src={logo} 
+                    onClick={() => nav('/')} 
+                    alt="만개레시피 제목" 
+                    style={{ width: 280, height: 90 }} 
+                />
                 <div id='searchContainer'>
-                    <input type="text" name='searchBox'  />
-                    <button><img src={SearchOk} alt="reading glasses" style={size}/></button>
+                    <input type="text" name='searchBox' />
+                    <button>
+                        <img src={SearchOk} alt="search icon" style={size} />
+                    </button>
                 </div>
                 <div id='iconContainer'>
-                <p>{user == null ? "" : user.displayName + " 님 환영합니다"}</p>
-                    
-                    <img onClick={()=>{
-                        // if(isLogin)
-                        // {
-                        //     ""
-                        // }
-                        // else{
-                        //     nav("/login");
-                        // }
-                        nav("/login")
-                    }} className='cursor' src={Profile} alt="프로필" style={size}/>
-                    <img className='cursor' src={MenuBtn} alt="메뉴" style={size}/>
+                    <p>{user ? `${user.displayName} 님 환영합니다` : ""}</p>
+                    <img 
+                        onClick={() => nav("/login")} 
+                        className='cursor' 
+                        src={Profile} 
+                        alt="프로필" 
+                        style={size} 
+                    />
+                    <img className='cursor' src={MenuBtn} alt="메뉴" style={size} />
                 </div>
             </div>
             <div id='menuContainer'>
                 <span className='cursor' onClick={() => nav('/')}>분류</span>
                 <span className='cursor' onClick={() => nav('/rank')}>랭킹</span>
-                
             </div>
         </div>
     );
--- src/components/replebox/RepleBox.tsx
+++ src/components/replebox/RepleBox.tsx
@@ -1,45 +1,57 @@
 import React, { useState } from 'react';
-import { ALLUSER, COMMENTS, REVIEW, setStarMenu, TOCOMMENTS } from '../../util/util';
+import { ALLUSER, COMMENTS, REVIEW, setStarMenu } from '../../util/util';
 
+interface RepleBoxProps {
+    type: string;
+}
 
-const RepleBox = ({type}) => {
-    const Style = {
-        marginLeft:40,
-        marginRight:20
-    }
-    if(type != ALLUSER)
-    {
+const RepleBox: React.FC<RepleBoxProps> = ({ type }) => {
+    const Style: React.CSSProperties = {
+        marginLeft: 40,
+        marginRight: 20,
+    };
+
+    if (type !== ALLUSER) {
         Style.pointerEvents = "none";
     }
-    
-    const [comdState, setComdState] = useState("");
-    const [score, setScore] = useState(0);
-    const [text, setText] = useState("");
+
+    const [comdState, setComdState] = useState<string>("");
+    const [score, setScore] = useState<number>(0);
+    const [text, setText] = useState<string>("");
 
     return (
-    <div className='container'>
-        <div className='flex_left flex_wrap_row_center' style={{marginLeft:60, marginRight:20}}>
-            <span>입력할 항목</span>
-            <select className='drop' onChange={(e)=>{
-                setComdState(e.target.value);
-                setScore(0);
-            }
-                } style={Style}>
-                <option value="댓글작성" selected={type == COMMENTS ? true : false}>댓글작성</option>
-                <option value="후기작성" selected={type == REVIEW ? true : false}>후기작성</option>
-            </select>
-            {comdState == "후기작성" ? setStarMenu(setScore) :""}
-
+        <div className='container'>
+            <div className='flex_left flex_wrap_row_center' style={{ marginLeft: 60, marginRight: 20 }}>
+                <span>입력할 항목</span>
+                <select
+                    className='drop'
+                    onChange={(e) => {
+                        setComdState(e.target.value);
+                        setScore(0);
+                    }}
+                    style={Style}
+                >
+                    <option value="댓글작성" selected={type === COMMENTS}>댓글작성</option>
+                    <option value="후기작성" selected={type === REVIEW}>후기작성</option>
+                </select>
+                {comdState === "후기작성" ? setStarMenu(setScore) : null}
+            </div>
+            <div className='fullWidth divFlex'>
+                <input
+                    className='deTailPut'
+                    type="text"
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button
+                    className='blueBtn bHover'
+                    onClick={() => {
+                        // 등록 로직 추가
+                    }}
+                >
+                    등록
+                </button>
+            </div>
         </div>
-        <div className='fullWidth divFlex'>
-            <input className='deTailPut' type="text" onChange={(e)=> setText(e.target.value)}/>
-            <button className='blueBtn bHover' onClick={() => {
-
-            }}>등록</button>
-
-        </div>
-
-    </div>
     );
 };
 
