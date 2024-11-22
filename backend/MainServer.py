a582d02791f17641dcd576ec489476ec80a89d6f
jw39 <akdlwb1212@naver.com>
2024-11-22 15:02:47 +0900

    지원 11/22 ssl 인증서 경로 수정

--- backend/MainServer.py
+++ backend/MainServer.py
@@ -11,15 +11,21 @@ from UploadBoard import upLoadRC
 from User import insertUser
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
-CORS(app)  # CORS 설정 추가
-# CORS(app, origins="http://localhost:3000")
+# CORS(app)  # CORS 설정 추가
+CORS(app, origins="http://localhost:3000")
 app.config['JSON_AS_ASCII'] = False
 api = Api(app)
 
 # SSL 인증서와 키 파일 설정 (ssl.SSLContext 사용)
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile='C:\\Users\\lee\\Desktop\\workspace\\recipy\\foodRecipe\\backend\\server-cert.pem',
-                        keyfile='C:\\Users\\lee\\Desktop\\workspace\\recipy\\foodRecipe\\backend\\private-key.pem')
+# context.load_cert_chain(certfile='C:\\Users\\lee\\Desktop\\workspace\\recipy\\foodRecipe\\backend\\server-cert.pem',
+#                         keyfile='C:\\Users\\lee\\Desktop\\workspace\\recipy\\foodRecipe\\backend\\private-key.pem')
+
+context.load_cert_chain(certfile='./server-cert.pem', keyfile='./private-key.pem')
+# 경로 확인 용
+# import os
+# print("Current working directory:", os.getcwd())
+
 
 # API 리소스 설정
 api.add_resource(AllInfo, '/all_info')
